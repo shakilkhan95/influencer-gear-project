d074a6dfe7db90304af12bb0feead3a385378096
shakilkhan95 <th.shakilkhan@gmail.com>
2026-01-26 13:42:09 +0400

    sponsor section styles added with responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const navigation = document.getElementById("navigation");
 const menuBar = document.getElementById("menu-bar");
-menuBar.onclick = () => {
+const navLinks = document.querySelectorAll('li');
+//Script to toggle nav items while clicking on hamburger icon
+menuBar.onclick = menuToggle;
+
+//Script to toggle nav items while clicking on nav links
+navLinks.forEach(link => {
+    link.onclick = menuToggle;
+})
+
+//Toggle Function
+function menuToggle() {
     navigation.classList.toggle('hidden');
 }

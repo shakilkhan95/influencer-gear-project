fe3232e5a74499421f59499273555d1ddd0aef92
shakilkhan95 <th.shakilkhan@gmail.com>
2026-01-31 02:05:24 +0400

    diverted to tailwindcss cli from vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,13 @@ module.exports = {
     "./**/*.{js,ts,jsx,tsx,html}",
     "./index.html"
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
 }
